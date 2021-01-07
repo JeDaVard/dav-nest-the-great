078044664565659101d22161ab1d6583d22d383a
Davit Vardanyan <jedavard@gmail.com>
2021-01-07 19:27:14 +0400

    check

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 
+console.log(process.env.POSTGRES_HOST, process.env.PORT);
 @Module({
     imports: [ConfigModule, TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
 })
